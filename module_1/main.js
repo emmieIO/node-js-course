60dc0fd0017c1ed02cd76d6ba21783261449387a
EmmieIO <markonuoha6@gmail.com>
2023-09-28 16:39:34 +0100

    javascript basics

---
+++ module_1/main.js
@@ -0,0 +1,12 @@
+var myName = "max";
+ var age = 25;
+ var hasHobbies = true; 
+
+function summerizeUser(userName, userAge, userHasHobby){
+    return {
+        userName, userAge, userHasHobby
+    }
+}
+ console.log(summerizeUser(myName, age, hasHobbies));
+
+ 
