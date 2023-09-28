aa942bd62e6e6dea5ed69308397eda73983384cd
EmmieIO <markonuoha6@gmail.com>
2023-09-28 16:53:19 +0100

    learing arrow function

--- module_1/main.js
+++ module_1/main.js
@@ -1,17 +1,26 @@
 var myName = "max";
 var age = 25;
-var hasHobbies = true; 
+var hasHobbies = true;
 
 // Javascript function block
-function summerizeUser(userName, userAge, userHasHobby){
-    return {
-        userName, userAge, userHasHobby
-    }
-}
- console.log(summerizeUser(myName, age, hasHobbies));
+// function summerizeUser(userName, userAge, userHasHobby){
+//     return {
+//         userName, userAge, userHasHobby
+//     }
+// }
+
+// converting the function block to an arrow function
+const summerizeUser = (userName, userAge, userHasHobby) => {
+  return {
+    userName,
+    userAge,
+    userHasHobby,
+  };
+};
+console.log(summerizeUser(myName, age, hasHobbies));
 
 //  there are next-gen js es syntax
-let newName =  "George";
+let newName = "George";
 const newAge = 45;
 let newHasHobbies = true;
 
